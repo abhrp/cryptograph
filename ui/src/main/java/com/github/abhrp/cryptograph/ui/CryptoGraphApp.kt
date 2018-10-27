1a3455eb28bff87ed6f5bdd9ffe7db3a012771e3
abhrp <abhiroop.patel@gmail.com>
2018-10-28 05:26:54 +0530

    Renamed app module to ui
    Signed-off-by: abhrp <abhiroop.patel@gmail.com>

--- ui/src/main/java/com/github/abhrp/cryptograph/ui/CryptoGraphApp.kt
+++ ui/src/main/java/com/github/abhrp/cryptograph/ui/CryptoGraphApp.kt
@@ -1,4 +1,4 @@
-package com.github.abhrp.cryptograph
+package com.github.abhrp.cryptograph.ui
 
 import android.app.Application
 import android.content.Context
--- ui/src/main/java/com/github/abhrp/cryptograph/ui/MainActivity.kt
+++ ui/src/main/java/com/github/abhrp/cryptograph/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.github.abhrp.cryptograph
+package com.github.abhrp.cryptograph.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.github.abhrp.cryptograph.R
 
 class MainActivity : AppCompatActivity() {
 

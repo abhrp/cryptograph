3bdfc2286f01794fdf7684a5a596e7f81c7789e7
abhrp <abhiroop.patel@gmail.com>
2018-11-29 13:45:05 +0530

    Changes to add test coverage reports

--- ui/src/main/java/com/github/abhrp/cryptograph/ui/CryptoGraphApp.kt
+++ ui/src/main/java/com/github/abhrp/cryptograph/ui/CryptoGraphApp.kt
@@ -4,11 +4,16 @@ import android.app.Activity
 import android.app.Application
 import android.content.Context
 import androidx.multidex.MultiDex
+import com.github.abhrp.cryptograph.BuildConfig
 import com.github.abhrp.cryptograph.ui.di.DaggerApplicationComponent
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasActivityInjector
 import javax.inject.Inject
+import com.instabug.library.invocation.InstabugInvocationEvent
+import com.instabug.library.Instabug
+
+
 
 class CryptoGraphApp : Application(), HasActivityInjector {
 
@@ -22,6 +27,13 @@ class CryptoGraphApp : Application(), HasActivityInjector {
     override fun onCreate() {
         super.onCreate()
         DaggerApplicationComponent.builder().application(this).build().inject(this)
+//        initInstaBug()
+    }
+
+    private fun initInstaBug() {
+        Instabug.Builder(this, BuildConfig.INSTABUG_KEY)
+            .setInvocationEvents(InstabugInvocationEvent.SHAKE, InstabugInvocationEvent.SCREENSHOT)
+            .build()
     }
 
     override fun attachBaseContext(base: Context?) {
